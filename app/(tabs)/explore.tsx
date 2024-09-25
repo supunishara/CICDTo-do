7b6a59adfcce46d4e53beb1deee75774ff95ee3e
Supun Ishara <supun.ishara@axiatadigitallabs.com>
2024-09-25 11:19:03 +0530

    second pull request

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -15,7 +15,7 @@ export default function TabTwoScreen() {
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">CI/CD NEW PAGE</ThemedText>
       </ThemedView>
-      <ThemedText>THIS IS For the new PUll Request</ThemedText>
+      <ThemedText>THIS IS For the second PUll Request</ThemedText>
     </ParallaxScrollView>
   );
 }
