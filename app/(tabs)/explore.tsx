91a8cbb217ae66cf21b1e6b2c7c6f170fad537f2
Supun Ishara <supun.ishara@axiatadigitallabs.com>
2024-09-26 11:02:07 +0530

    new text change

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -15,7 +15,7 @@ export default function TabTwoScreen() {
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">CI/CD NEW PAGE</ThemedText>
       </ThemedView>
-      <ThemedText>THIS IS For the seventh PUll Request</ThemedText>
+      <ThemedText>THIS IS For the eight PUll Request</ThemedText>
     </ParallaxScrollView>
   );
 }
